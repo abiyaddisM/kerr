6b1600c4c6b4fa4721cef6a5e31e3462b5f46a73
Abiy Addis <abiymw17@gmail.com>
2024-10-01 18:29:37 +0300

    [Organize]

--- src/utils/ProtectedRoutes.jsx
+++ src/utils/ProtectedRoutes.jsx
@@ -3,8 +3,8 @@ import PrivateLayout from "../layouts/Private Layout/PrivateLayout.jsx";
 import {useAuth} from "./AuthContext.jsx";
 
 export const ProtectedRoutes = () => {
-    const { isAuthenticated } = useAuth();
-
+    // const { isAuthenticated } = useAuth();
+    const isAuthenticated = true;
     return isAuthenticated
         ?
         <PrivateLayout>
--- src/utils/PublicRoutes.jsx
+++ src/utils/PublicRoutes.jsx
@@ -2,7 +2,9 @@ import {Navigate, Outlet} from "react-router-dom";
 import {useAuth} from "./AuthContext.jsx";
 
 export const PublicRoutes = () => {
-    const { isAuthenticated } = useAuth();
+    // const { isAuthenticated } = useAuth();
+    const isAuthenticated = true;
+
     return isAuthenticated
         ?
             <Navigate to={'/'}/>
