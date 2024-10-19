be39371d45f670352e643fb757c17f5b12745aaa
Abiy Addis <abiymw17@gmail.com>
2024-10-19 14:24:40 +0300

    [Organize]

--- src/components/general/Profile Image/ProfileImage.jsx
+++ src/components/general/Profile Image/ProfileImage.jsx
@@ -30,7 +30,7 @@ const ProfileImage = ({userId, src='', size='28px'}) => {
     return(
             <img 
             className={styles.profile_image}
-            src={src || `https://auth.bizawit.com/api/v1/upload/original/${user.profile_picture}`} 
+            src={`https://auth.bizawit.com/api/v1/upload/600/${src}`}
             style={{ width:size, height:size }}
             onClick={()=>handelProfileClick(userId)}
         />
